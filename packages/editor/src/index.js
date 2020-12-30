40f7dec71aea0be7a8f07580d7099c808327fec8
ftun <felipe.tun.cauich@gmail.com>
2020-12-30 10:21:37 -0500

    mod

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -1,30 +1,45 @@
 import React, { useState, useEffect, useRef } from 'react';
-
+import { createPortal } from 'react-dom';
 import { BtnToolBar } from './toolBar';
 import './style.css';
 
 const styleTextBox = {
-    // width: 540,
+    width: '100%',
     height: 200,
     border:' 1px #000000 solid',
-    padding: 12,
-    overflow: 'scroll',
+    // padding: 12,
+    // overflow: 'scroll',
 };
 
 const Editor = props => {
-    const oDoc = useRef(null);
+    const refIframe = useRef(null);
+    const [value, setValue] = useState('');
+
+    useEffect(() => {
+        if (refIframe.current) {
+            refIframe.current.contentDocument.body.contentEditable = true;
+            refIframe.current.contentDocument.body.onkeyup = () => {
+                setValue(refIframe.current.contentWindow.document.body.innerHTML);
+            };
+
+            refIframe.current.paste = e => {
+                console.log('paste', e);
+            }
+        }
+    }, []);
 
     const setModeContent = (e) => {
-        console.log(oDoc.current);
+        console.log('value', value);
+        // console.log(oDoc.current);
     };
 
     const setFormatDocument = (sCmd, sValue) => {
-        document.execCommand(sCmd, false, sValue); oDoc.current.focus();
-      // if (validateMode()) { document.execCommand(sCmd, false, sValue); oDoc.focus(); }
+        refIframe.current.contentWindow.document.execCommand(sCmd, false, sValue); refIframe.current.contentWindow.document.body.focus();
+        setValue(refIframe.current.contentWindow.document.body.innerHTML);
+        // console.log(refIframe.current.contentWindow.document.body.innerHTML);
     }
-    // useEffect() => {
-    //
-    // });
+
+    // const getOnChange = () => console.log(refIframe.current.contentWindow.document.body.innerHTML);
 
     // <img title="Hyperlink" onClick="var sLnk=prompt('Write the URL here','http:\/\/');if(sLnk&&sLnk!=''&&sLnk!='http://'){setFormatDocument('createlink',sLnk)}" src={BtnToolBar.Hyperlink} />
 
@@ -56,9 +71,7 @@ const Editor = props => {
                 </div>
             </div>
             <div className="row">
-                <div ref={oDoc} style={styleTextBox} contentEditable="true" suppressContentEditableWarning={true} className="col s12 m12">
-                    <p>Lorem ipsum</p>
-                </div>
+                <iframe ref={refIframe} style={styleTextBox} contentEditable="true" suppressContentEditableWarning={true}>{value}</iframe>
             </div>
             <div className="row">
                 <p>
@@ -68,6 +81,46 @@ const Editor = props => {
             </div>
         </form>
     </div>;
+};
+
+// const iframeStyles = `
+// body {
+//   margin: 0;
+//   font-family: 'Helvetica Neue', Helvetica, sans-serif;
+// }
+// [contenteditable="true"] {
+//   width: 100%;
+//   height: 100vh;
+//   padding: 10px;
+//   box-sizing: border-box;
+//   font-size: 1.3rem;
+//   line-height: 1.35;
+// }`;
+
+const Iframe = props => {
+    const [ref, setRef] = useState(null);
+    const refIframe = useRef(null);
+
+    useEffect(() => {
+        if (refIframe.current) {
+            setRef(refIframe.current);
+            refIframe.current.contentDocument.body.contentEditable = true;
+            // refIframe.current.contentWindow.document.body.contentEditable='true';
+            // refIframe.current.contentDocument.head.innerHTML = `<style>${iframeStyles}</style>`;
+        }
+    }, []);
+
+    // onload = e => {
+    //   the_frame.onload = e => {the_frame.contentDocument.body.contentEditable = true;}
+    //   the_frame.src = URL.createObjectURL(new Blob([`<html><body>An other doc</body></html>`], {type: 'text/html'}));
+    // };
+
+    return <iframe ref={refIframe} />
+    // return (<React.Fragment>
+    //         <iframe ref={refContainer} />
+    //         {ref && ref.contentDocument && createPortal(props.children, ref.contentDocument.body)}
+    //     </React.Fragment>
+    // );
 };
 
 
